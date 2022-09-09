cd977f78fdad8ac20f8e6f7d6f2bae413bfba9d7
ceno <cenomail@gmail.com>
2022-09-09 19:20:59 +0100

    Force type to nano
    In theory it was meant to pick the cheapest of the two, but for some in
    California it's always picking micro even though it's clear double the price.

--- cdk/scripts/s3sync.sh
+++ cdk/scripts/s3sync.sh
@@ -2,6 +2,7 @@
 
 S3_BUCKET_LIST=$(npx aws-cdk list --long --json | jq -r ".[].id | ascii_downcase")
 
-echo "$S3_BUCKET_LIST" | xargs -t -I {} aws s3 sync --dryrun --size-only --exclude ".DS_Store" ../dist s3://{}
+echo "$S3_BUCKET_LIST" | xargs -t -I {} aws s3 sync --size-only --exclude ".DS_Store" ../dist s3://{}
+echo "ceno-quake3-paris" | xargs -t -I {} aws s3 sync --size-only --exclude ".DS_Store" ../dist s3://{}
 
 echo dont forget to manually sync the buckets NOT managed by cdk
